f2b3156885ddf8359dd5e458ee00b6ecc8babfc5
Sam Wlody <swlody@gmail.com>
2025-01-28 21:43:51 -0500

    add http.status_code to sentry trace

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use sentry::protocol::SpanStatus;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -24,11 +25,19 @@ pub enum RrgError {
 
 impl IntoResponse for RrgError {
     fn into_response(self) -> Response {
-        if let Self::Uuid(_) | Self::ToStr(_) | Self::BadRequest = self {
-            StatusCode::BAD_REQUEST.into_response()
-        } else {
-            tracing::error!("{:?}", self);
-            StatusCode::INTERNAL_SERVER_ERROR.into_response()
-        }
+        let (http_status, sentry_status) =
+            if let Self::Uuid(_) | Self::ToStr(_) | Self::BadRequest = self {
+                (StatusCode::BAD_REQUEST, SpanStatus::InvalidArgument)
+            } else {
+                tracing::error!("{:?}", self);
+                (StatusCode::INTERNAL_SERVER_ERROR, SpanStatus::InternalError)
+            };
+
+        sentry::configure_scope(|scope| {
+            scope.get_span().map(|span| span.set_status(sentry_status));
+            scope.set_tag("http.status_code", http_status.as_u16());
+        });
+
+        http_status.into_response()
     }
 }
